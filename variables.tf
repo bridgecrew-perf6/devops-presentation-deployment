f83c59bee6c3770917ecc490004d7b6b4454dc3b
root <juanma.cvega@gmail.com>
2022-04-13 09:28:48 +0000

    Update service to version b07e2ed7f7b37b8f0005a3d41a2cef3206fd9a36

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@ variable "devops_presentation" {
     service = {
       cpu_units     = 256
       image_name    = "juanmacvega/devops-presentation"
-      image_version = "ffb5ffc035cb249a2ae88b98b1ab05d95995ddb5"
+      image_version = "b07e2ed7f7b37b8f0005a3d41a2cef3206fd9a36"
       ingress = {
         port     = 8080
         protocol = "TCP"
